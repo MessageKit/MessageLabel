4a91005ef4c2afff2135a3d568169f4db210d399
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-10-20 04:25:58 -0500

    Setup podspec

---
+++ Sources/MessageLabel.swift
@@ -0,0 +1,11 @@
+//
+//  MessageLabel.swift
+//  MessageLabel
+//
+//  Created by Steven on 10/20/17.
+//  Copyright © 2017 MessageKit. All rights reserved.
+//
+
+import Foundation
+
+class MessageLabel {}
