99cc81756ebe6c8dfa45b83272551139df646305
Steven Deutsch <stevensdeutsch@yahoo.com>
2017-10-20 03:55:28 -0500

    Add MessageLabel podspec

---
+++ MessageLabel/MessageLabel/MessageLabel.h
@@ -0,0 +1,19 @@
+//
+//  MessageLabel.h
+//  MessageLabel
+//
+//  Created by Steven on 10/20/17.
+//  Copyright © 2017 MessageKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessageLabel.
+FOUNDATION_EXPORT double MessageLabelVersionNumber;
+
+//! Project version string for MessageLabel.
+FOUNDATION_EXPORT const unsigned char MessageLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageLabel/PublicHeader.h>
+
+
